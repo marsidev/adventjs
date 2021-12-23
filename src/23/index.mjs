2d1c15a0844e8e4f8abd73feeec54e2032831461
Luis Marsiglia <luismarsiglia19@gmail.com>
2021-12-23 14:11:50 -0400

    Challenge 23 code optimized

--- src/23/index.mjs
+++ src/23/index.mjs
@@ -1,4 +1,4 @@
 export default function canReconfigure(from, to) {
-  function removeRepeated(str) { return [...new Set(str.split(''))].join('') }
+  const removeRepeated = str => [...new Set(str.split(''))].join('')
   return from.length === to.length && removeRepeated(from).length === removeRepeated(to).length
 }
