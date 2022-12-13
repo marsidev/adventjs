569b06b585c59f3b2765915afa6968c695423eb8
Luis Marsiglia <marsiglia.business@gmail.com>
2022-12-13 15:17:44 -0400

    improve solution

--- 2022/07/index.mjs
+++ 2022/07/index.mjs
@@ -1,11 +1,6 @@
 export function getGiftsToRefill(a1, a2, a3) {
-	return [
-		...new Set([
-			...a1.filter(g => [...a2, ...a3].indexOf(g) === -1),
-			...a2.filter(g => [...a1, ...a3].indexOf(g) === -1),
-			...a3.filter(g => [...a1, ...a2].indexOf(g) === -1)
-		])
-	]
+	const allGifts = [...a1, ...a2, ...a3]
+	return [...new Set(allGifts)].filter(g => a1.includes(g) + a2.includes(g) + a3.includes(g) === 1)
 }
 
 // score: 200
